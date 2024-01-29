eb34e4e53a82c452326aa877fdb502c2e09b7d50
OlubodeMichael <moolubode3@gmail.com>
2024-01-29 10:29:39 -0500

    project list data seperated

---
+++ src/components/projectList.js
@@ -0,0 +1,19 @@
+import Project1 from "../assest/project1.png";
+import Project2 from "../assest/project2.png";
+
+export const projectList = [
+  {
+    name: 'Eat-N-Split',
+    link: 'https://eat-n-split-3pg1xuswj-michael-olubodes-projects.vercel.app/',
+    describe: 'This React application enables users to manage expenses and balances with friends, including adding friends, selecting them for bill splitting, and updating balances, fostering easy expense tracking and financial management.',
+    img: Project1,
+    github: 'https://github.com/OlubodeMichael/Eat-N-Split'
+  },
+  {
+    name: 'Url-Shortener',
+    link: 'https://url-shortener-peach.vercel.app/',
+    describe: 'The link shortening functionality is achieved through an intuitive interface that utilizes the "https://api.shrtco.de" API for dynamic results, integrating advanced analytics and branding features. Conversely, the QR code generator enables users to input links, generating QR codes instantly within a clean and minimalist design.',
+    img: Project2,
+    github: 'https://github.com/OlubodeMichael/Url-Shortener'
+  },
+];
