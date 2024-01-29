c35d15d235e1281fd9123461f1e798551b3886a8
OlubodeMichael <moolubode3@gmail.com>
2024-01-29 10:47:31 -0500

    Animation added to the navbar

--- src/App.js
+++ src/App.js
@@ -5,22 +5,31 @@ import { Scroll } from "./components/ScrollToTop";
 import { Project } from "./components/Project";
 import { NavBar } from "./Pages/NavBar";
 import { HeroSection } from "./Pages/HeroSection";
+import { motion, useScroll } from "framer-motion";
 
 function App() {
   const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const { scrollYProgress } = useScroll();
   function handleMenu() {
     setMenuIsOpen((isOpen) => !isOpen)
   }
   return (
-    <div className="min-h-screen flex flex-col  text-white scroll-smooth focus:scroll-auto">
-      {!menuIsOpen && <Scroll />}
-      <NavBar handleMenu={handleMenu} menuIsOpen={menuIsOpen}/>
-      <HeroSection />
-      <About />
-      <Project />
-      <Contact />
+    <>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
+      <div className="min-h-screen flex flex-col  text-white scroll-smooth focus:scroll-auto">
+        {!menuIsOpen && <Scroll />}
+        
+        <NavBar handleMenu={handleMenu} menuIsOpen={menuIsOpen}/>
+        <HeroSection />
+        <About />
+        <Project />
+        <Contact />
 
-    </div>
+      </div>
+    </>
   );
 }
 
